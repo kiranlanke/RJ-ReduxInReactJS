1d23ba8bfb172b58c0712a15c80b81804d5daef4
Kiranlanke <klanke@ardxx.com>
2022-09-01 00:02:26 +0530

    Redux in ReactJS

---
+++ src/redux/reducers/counterReducer.js
@@ -0,0 +1,22 @@
+const initialState = {
+	count: 0,
+};
+
+export const counterReducer = (state = initialState, action) => {
+	switch (action.type) {
+		case 'INCREMENT':
+			return {
+				...state,
+				count: state.count + action.payload
+			};
+	}
+	switch (action.type) {
+		case 'DECREMENT':
+			return {
+				...state,
+				count: state.count - action.payload
+			};
+		default:
+			return state;
+	}
+};
